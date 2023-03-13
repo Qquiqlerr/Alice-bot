aba4a05145a58cfcec3cd606941e08a5796f6c9f
Qquiqlerr <101799376+Qquiqlerr@users.noreply.github.com>
2023-03-13 21:45:44 +0300

    Hotfix 1
    -The automatic horoscope sending function has been removed
    -Horoscope request button added

--- bot.py
+++ bot.py
@@ -1,18 +1,15 @@
-import datetime
 import requests
 from bs4 import BeautifulSoup
 from telebot.async_telebot import AsyncTeleBot
 import openai
 from telebot import types
 import random
-import asyncio
 import os
 import json
 import io
-
+import asyncio
 #var
-PROMPT = 'cute cat in the forest'
-timehoro1 = datetime.time(8,30)
+PROMPT = 'Cute kitten in the house'
 
 def startupCheckTelegramApi():
     if os.path.isfile('key_tg_api.json') and os.access('key_tg_api.json', os.R_OK):
@@ -40,6 +37,11 @@ def startupCheckOpenaiApi():
         with io.open(os.path.join('', 'key_openai.json'), 'w') as db_file:
             db_file.write(json.dumps(key_))
 
+def sendhoroscope():
+    url = ("https://horoscopes.rambler.ru/capricorn")
+    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+    return soup.find("div", class_="_1E4Zo _3BLIa").text
+
 #API
 bot = AsyncTeleBot(startupCheckTelegramApi())
 openai.api_key = startupCheckOpenaiApi()
@@ -48,10 +50,12 @@ openai.api_key = startupCheckOpenaiApi()
 send_comp = types.KeyboardButton('Получить комплимент')
 send_cat = types.KeyboardButton('Получить котика')
 send_NeuroCat = types.KeyboardButton('Получить нейрокотика')
+send_horo = types.KeyboardButton('Получить гороскоп на сегодня')
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 markup.add(send_cat)
 markup.add(send_comp)
 markup.add(send_NeuroCat)
+markup.add(send_horo)
 #path
 path = ['Cats/1628760997_16-p-foto-kotyat-milikh-i-nyashnikh-17.jpg',
         'Cats/1628904992_46-p-skachat-foto-milikh-kotikov-50.jpg',
@@ -62,20 +66,11 @@ path = ['Cats/1628760997_16-p-foto-kotyat-milikh-i-nyashnikh-17.jpg',
         'Cats/kotenok-koshka-kot.jpg']
 
 
+
+
 @bot.message_handler(commands=["start"])
 async def return_text_digit(msg):
     await bot.send_message(msg.chat.id, 'Что ты хочешь?)', reply_markup=markup)
-    while True:
-        url = ("https://horoscopes.rambler.ru/capricorn")
-        soup = BeautifulSoup(requests.get(url).content, "html.parser")
-        await asyncio.sleep(60)
-        date_time_current = datetime.datetime.now()
-        time_now = date_time_current.time()
-        if time_now.hour == timehoro1.hour and time_now.minute == timehoro1.minute:
-            await bot.send_voice()
-            await bot.send_message(msg.chat.id,soup.find("div", class_="_1E4Zo _3BLIa").text)
-
-
 
 
 @bot.message_handler(content_types=["text"])
@@ -88,13 +83,19 @@ async def return_text_digit(msg):
         photo = open(path[random.randrange(7)],'rb')
         await bot.send_photo(msg.chat.id,photo)
         photo.close()
-    else:
+    elif msg.text == 'Получить нейрокотика':
         response = openai.Image.create(
             prompt=PROMPT,
             n=1,
             size="256x256",
         )
-        await bot.send_photo(msg.chat.id,response["data"][0]["url"])
+        await bot.send_photo(msg.chat.id, response["data"][0]["url"])
+    elif msg.text == 'Получить гороскоп на сегодня':
+        await bot.send_message(msg.chat.id, f'Алиса, вот твой гороскоп на сегодня❤️💕\n'
+                                            f'{sendhoroscope()}')
+
+
+
 
 
 asyncio.run(bot.infinity_polling())
